7f3738c5bdeed88d71f58ade8ec4f0fad39adc92
Edison UWIHANGANYE <edsnkvn@gmail.com>
2024-06-13 11:04:09 +0200

    day 1 of our fullstack camp

---
+++ JS/click.js
@@ -0,0 +1,21 @@
+function clicked(){
+    let demoelt = document.getElementById('demo')
+    demoelt.innerHTML= "clicked"
+
+}
+function sayName(){
+    let name = document.getElementById('name').value;
+    alert("hello " + name);
+}
+function message(age){
+    console.log("you have " + age + " years old");
+}
+message(40);
+
+function hide(){
+
+    // let  btn = document.getElementById('btn')
+    let  div = document.getElementById('mydiv')
+    div.style.backgroundColor = 'blue';
+
+}
---
+++ JS/home.js
@@ -0,0 +1,29 @@
+console.log("hello tis our js code "); // this is first way of dispay data in js
+// alert("welcome"); // 2nd way of display data
+// document.write("this is document out put") // 3rd way of display data in js
+
+// let domelt = document.getElementById("dom")
+// domelt.innerHTML ="this is javascript content";
+
+//
+const PI = 3.14
+var age = 12;
+age = 10;
+// PI = 20
+console.log(PI);
+// let 
+// const
+{
+    const PI = 3.14
+    let x = 10
+console.log(x+PI);
+}
+    let x = 20;
+
+    // using function
+
+    function sayHello(){
+        console.log("hello world");
+    }
+    
+    sayHello();
